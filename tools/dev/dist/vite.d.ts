bdf443d6f2dd5937c7d3d7ca62eb2ace5a64e1f0
Irshad Ahmad <94346512+irshadahmad21@users.noreply.github.com>
2023-12-11 18:01:14 +0530

    Set up turbo (#2)
    * Init turbo
    
    * Clear dist
    
    * Fix formatting
    
    * Fix lints

--- tools/dev/dist/vite.d.ts
+++
@@ -1,5 +0,0 @@
-import { V4wpOptions } from '@kucrut/vite-for-wp';
-import { UserConfig, defineConfig } from 'vite';
-export { defineConfig };
-export declare function createViteConfig(options: V4wpOptions): UserConfig;
-//# sourceMappingURL=vite.d.ts.map
