cd101bb456017efdcfe8d1f4f77a8f2c3d390a9c
Irshad Ahmad <irshad@wpsocio.com>
2023-12-26 22:22:03 +0530

    Clean up dev package

--- tools/dev/vite.js
+++ tools/dev/vite.js
@@ -1,10 +1,6 @@
 // @ts-check
-import { v4wp } from '@kucrut/vite-for-wp';
-import { wp_scripts } from '@kucrut/vite-for-wp/plugins';
-import react from '@vitejs/plugin-react';
+import viteWpReact from '@wpsocio/vite-wp-react';
 import { defineConfig } from 'vite';
-import { Plugin as importToCDN } from 'vite-plugin-cdn-import';
-import { extractExternalDepsPlugin } from './extract-external-deps-plugin.js';
 
 export { defineConfig };
 
@@ -15,58 +11,21 @@ export { defineConfig };
  *
  * @returns {import('vite').UserConfigExport}
  */
-export function createViteConfig({ outDir, input, buildOptions, makePot }) {
-	return function config({ command }) {
-		/**
-		 * @type {import('@vitejs/plugin-react').BabelOptions | undefined}
-		 */
-		let babel;
-
-		// Add makepot plugin if command is build and makePot is defined
-		if (command === 'build' && makePot) {
-			babel = {
-				plugins: [['@wordpress/babel-plugin-makepot', makePot]],
-			};
-		}
-
-		return {
-			plugins: [
-				react({ babel }),
-				v4wp({ outDir, input }),
-				wp_scripts(),
-				importToCDN({
-					/**
-					 * Use the fake CDN imports to ensure react imports don't end up in the bundle
-					 * framer-motion causes the issue by using namespace imports
-					 *
-					 * @see https://github.com/vitejs/vite-plugin-react/issues/3
-					 */
-					modules: [
-						{
-							name: 'react',
-							var: 'React',
-							path: 'https://wpsocio.com',
-						},
-						{
-							name: 'react-dom',
-							var: 'ReactDOM',
-							path: 'https://wpsocio.com',
-						},
-					],
-				}),
-				extractExternalDepsPlugin(),
+export function createViteConfig({ outDir, input, makePot }) {
+	return {
+		build: {
+			assetsDir: 'dist',
+		},
+		plugins: [
+			viteWpReact(
+				{ outDir, input },
 				{
-					name: 'wpsocio:override-config',
-					config: () => ({
-						build: {
-							assetsDir: 'dist',
-							// ensure that manifest.json is not in ".vite/" folder
-							manifest: 'manifest.json',
-							...buildOptions,
-						},
-					}),
+					extractWpDependencies: {},
+					externalizeWpPackages: true,
+					enableReact: true,
+					makePot,
 				},
-			],
-		};
+			),
+		],
 	};
 }
