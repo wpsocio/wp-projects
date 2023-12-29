599421398425c1ec914459a8544be196187ec6e7
Irshad Ahmad <94346512+irshadahmad21@users.noreply.github.com>
2023-12-29 14:37:53 +0530

    Create and use base project command (#24)
    * Fix include flag for CLI clean command
    
    * Create and use base command

--- tools/cli/src/commands/unlink.ts
+++ tools/cli/src/commands/unlink.ts
@@ -1,64 +1,26 @@
-import { Args, Flags } from '@oclif/core';
 import chalk from 'chalk';
-import {
-	getMonorepoProjects,
-	getSymlinkPath,
-	normalizeProjectsInput,
-	validateProject,
-} from '../utils/projects.js';
-import { SymlinkManager } from '../utils/symlinks.js';
+import { getSymlinkPath } from '../utils/projects.js';
 import Link from './link.js';
 
 export default class Unlink extends Link {
 	static description =
 		'Removes symlinks in the given wp-content directory created for the project(s) in this monorepo.';
 
-	static flags = {
-		...Link.flags,
-		all: Flags.boolean({
-			description: 'Unlink all projects.',
-		}),
-	};
-
-	static args = {
-		...Link.args,
-		projects: Args.string({
-			description: 'Project(s) to unlink',
-		}),
-	};
-
-	static strict = false;
-
-	getInput() {
-		return this.parse(Unlink);
-	}
-
 	public async run(): Promise<void> {
-		const { args, flags, raw } = await this.getInput();
-
 		try {
-			this.assertArgs(args, flags);
-
-			const symlinkManager = new SymlinkManager();
-
-			const projects = flags.all
-				? getMonorepoProjects()
-				: normalizeProjectsInput(raw);
-
-			for (const project of projects) {
-				validateProject(project);
-			}
-
-			if (flags.all) {
-				symlinkManager.setMessages({
+			if (this.flags.all) {
+				this.symlinkManager.setMessages({
 					linkDoesNotExist: '',
 				});
 			}
 
-			for (const project of projects) {
-				const symlinkPath = getSymlinkPath(project, flags['wp-content-dir']);
+			for (const project of this.projects) {
+				const symlinkPath = getSymlinkPath(
+					project,
+					this.flags['wp-content-dir'],
+				);
 
-				const result = symlinkManager.removeSymlink({ symlinkPath });
+				const result = this.symlinkManager.removeSymlink({ symlinkPath });
 
 				if (result) {
 					this.log(result);
