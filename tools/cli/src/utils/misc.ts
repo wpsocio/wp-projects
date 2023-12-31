14d2e2858b42a7b034fed2cdacdb35fbb8fb58e1
Irshad Ahmad <irshad@wpsocio.com>
2023-12-31 15:29:50 +0530

    Update misc.ts

--- tools/cli/src/utils/misc.ts
+++ tools/cli/src/utils/misc.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import fg, { Options } from 'fast-glob';
 
 export type ToUpdate = {
@@ -10,4 +12,26 @@ export function globFiles(toUpdate: ToUpdate, options?: Options) {
 		ignore: toUpdate.ignore,
 		...options,
 	});
+}
+
+export async function copyDir(
+	sourceDir: string,
+	destDir: string,
+	options?: Partial<Options>,
+) {
+	const entries = globFiles(
+		{
+			ignore: ['**/node_modules/**'],
+			files: ['**/*'],
+			...options,
+		},
+		{ cwd: sourceDir },
+	);
+
+	for (const file of entries) {
+		const filePath = path.join(sourceDir, file);
+		const destPath = path.join(destDir, file);
+
+		fs.copyFileSync(filePath, destPath);
+	}
 }
