9a7de44dfa8fec65fef203baa77d7d711aede629
Irshad Ahmad <irshad@wpsocio.com>
2024-01-05 16:53:43 +0530

    Clean up

--- tools/cli/src/hooks/init/setup.ts
+++
@@ -1,9 +0,0 @@
-import { Hook } from '@oclif/core';
-import { ROOT_DIR } from '../../utils/monorepo.js';
-
-const hook: Hook<'init'> = async () => {
-	// Fix the CLI location to the root of the monorepo.
-	process.chdir(ROOT_DIR);
-};
-
-export default hook;
