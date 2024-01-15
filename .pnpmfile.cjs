cc66161048b2c2de439905a4d2e39d0dad08e573
Irshad Ahmad <irshad@wpsocio.com>
2024-01-15 18:09:34 +0530

    Update .pnpmfile.cjs

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,3 +1,42 @@
+function fixPeerDeps(pkg) {
+	const reactOldPkgs = new Set([
+		'react-autosize-textarea',
+		'reakit',
+		'reakit-system',
+		'reakit-utils',
+		'reakit-warning',
+	]);
+	if (reactOldPkgs.has(pkg.name)) {
+		for (const p of ['react', 'react-dom']) {
+			if (!pkg.peerDependencies?.[p]) {
+				continue;
+			}
+
+			if (
+				pkg.peerDependencies[p].match(/(?:^|\|\|\s*)(?:\^16|16\.x)/) &&
+				!pkg.peerDependencies[p].match(/(?:^|\|\|\s*)(?:\^17|17\.x)/)
+			) {
+				pkg.peerDependencies[p] += ' || ^17';
+			}
+			if (
+				pkg.peerDependencies[p].match(/(?:^|\|\|\s*)(?:\^17|17\.x)/) &&
+				!pkg.peerDependencies[p].match(/(?:^|\|\|\s*)(?:\^18|18\.x)/)
+			) {
+				pkg.peerDependencies[p] += ' || ^18';
+			}
+		}
+	}
+
+	return pkg;
+}
+
+function readPackage(pkg, context) {
+	if (pkg.name) {
+		return fixPeerDeps(pkg, context);
+	}
+	return pkg;
+}
+
 function afterAllResolved(lockfile) {
 	// Since we are not tracking the lockfile, we need to generate a hash of the lockfile
 	// To use for cache busting
@@ -18,6 +57,7 @@ function afterAllResolved(lockfile) {
 
 module.exports = {
 	hooks: {
+		readPackage,
 		afterAllResolved,
 	},
 };
