5bddab037ac52c89a0aa8c91f9e8c53abb3edf13
Irshad Ahmad <irshad@wpsocio.com>
2024-01-23 21:00:14 +0530

    Update project config

--- plugins/wptelegram-login/wpdev.project.js
+++ plugins/wptelegram-login/wpdev.project.js
@@ -3,10 +3,8 @@
  * @type {import("@wpsocio/wpdev").ProjectConfig['getProjectInfo']}
  */
 export const getProjectInfo = () => ({
-	title: 'WP Telegram Login',
 	key: 'wptelegram_login',
 	slug: 'wptelegram-login',
-	textDomain: 'wptelegram-login',
 });
 
 export { getBundleConfig } from '../wpdev.base.project.js';
--- plugins/wptelegram-widget/wpdev.project.js
+++ plugins/wptelegram-widget/wpdev.project.js
@@ -3,10 +3,8 @@
  * @type {import("@wpsocio/wpdev").ProjectConfig['getProjectInfo']}
  */
 export const getProjectInfo = () => ({
-	title: 'WP Telegram Widget',
 	key: 'wptelegram_widget',
 	slug: 'wptelegram-widget',
-	textDomain: 'wptelegram-widget',
 });
 
 export { getBundleConfig } from '../wpdev.base.project.js';
