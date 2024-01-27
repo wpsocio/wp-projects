13c281b8edda410d7f31fdd8b4d20715de339c6d
Irshad Ahmad <irshad@wpsocio.com>
2024-01-27 12:01:08 +0530

    Fix npm types for wpdev for CJS imports

---
+++ tools/wpdev/tsup.config.ts
@@ -0,0 +1,17 @@
+import { Options, defineConfig } from 'tsup';
+
+const commonConfig: Options = {
+	clean: true,
+	splitting: true,
+	dts: true,
+	sourcemap: true,
+	format: ['cjs'],
+	outDir: 'dist/cjs',
+};
+export default defineConfig([
+	{
+		entry: ['src/index.ts'],
+		...commonConfig,
+		name: 'index',
+	},
+]);
