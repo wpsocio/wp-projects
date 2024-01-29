ffab82ab387ac09b6a95ab5c06131c9d4220a7b3
Irshad Ahmad <irshad@wpsocio.com>
2024-01-29 09:11:57 +0530

    Commit lockfile

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -42,27 +42,8 @@ function readPackage(pkg, context) {
 	return pkg;
 }
 
-function afterAllResolved(lockfile) {
-	// Since we are not tracking the lockfile, we need to generate a hash of the lockfile
-	// To use for cache busting
-	const hashedContent = require('crypto')
-		.createHash('sha1')
-		.update(JSON.stringify(lockfile))
-		.digest('hex');
-
-	console.log('hashedContent', hashedContent);
-
-	require('fs').writeFileSync(
-		require('path').join(__dirname, 'pnpm-hashed.lock'),
-		hashedContent,
-	);
-
-	return lockfile;
-}
-
 module.exports = {
 	hooks: {
 		readPackage,
-		afterAllResolved,
 	},
 };
