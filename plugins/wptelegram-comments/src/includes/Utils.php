44e433aefcec4a3d0ab5470485ca1b30b40a0a22
Irshad Ahmad <irshad@wpsocio.com>
2024-08-01 02:44:29 +0530

    Restore translations for plugin name after l10n fix (#150)
    * Restore translations for plugin name
    
    * Add changelog

--- plugins/wptelegram-comments/src/includes/Utils.php
+++ plugins/wptelegram-comments/src/includes/Utils.php
@@ -63,8 +63,8 @@ class Utils extends \WPSocio\WPUtils\Helpers {
 
 		if ( ! defined( 'WPTELEGRAM_LOADED' ) && empty( $admin_page_hooks['wptelegram'] ) ) {
 			add_menu_page(
-				'WP Telegram',
-				'WP Telegram',
+				__( 'WP Telegram', 'wptelegram-comments' ),
+				__( 'WP Telegram', 'wptelegram-comments' ),
 				'manage_options',
 				'wptelegram',
 				null,
