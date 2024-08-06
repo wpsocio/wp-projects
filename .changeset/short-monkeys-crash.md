d14a2f9f13087a18c184a0ca23a2f981288ffe5e
Irshad Ahmad <irshad@wpsocio.com>
2024-08-06 10:53:55 +0530

    Fix Message Template info not expanding (#156)

---
+++ .changeset/short-monkeys-crash.md
@@ -0,0 +1,5 @@
+---
+"wptelegram": patch
+---
+
+Fixed Message Template info not expanding
--- packages/js/components/collapse/Collapse.tsx
+++ packages/js/components/collapse/Collapse.tsx
@@ -1,11 +1,4 @@
-import {
-	Accordion,
-	AccordionButton,
-	AccordionIcon,
-	AccordionItem,
-	AccordionPanel,
-	Box,
-} from '@wpsocio/adapters';
+import { Box } from '@wpsocio/adapters';
 
 export interface CollapseProps {
 	title: string;
@@ -18,18 +11,11 @@ export const Collapse: React.FC<React.PropsWithChildren<CollapseProps>> = ({
 	title,
 }) => {
 	return (
-		<Accordion allowToggle>
-			<AccordionItem>
-				<AccordionButton>
-					<Box flex="1" textAlign="start">
-						{title}
-					</Box>
-					<AccordionIcon />
-				</AccordionButton>
-				<AccordionPanel>
-					<Box>{body || children}</Box>
-				</AccordionPanel>
-			</AccordionItem>
-		</Accordion>
+		<details style={{ borderBlock: '1px solid #E2E8F0' }}>
+			<summary style={{ paddingBlock: '0.5rem', cursor: 'pointer' }}>
+				{title}
+			</summary>
+			<Box paddingBottom="1rem">{body || children}</Box>
+		</details>
 	);
 };
