1aea95b1a97ed6019deca7745d546023df87e21e
Irshad Ahmad <irshad@wpsocio.com>
2025-04-01 10:04:14 +0530

    Use the externals callback for development

--- tools/vite-wp-react/src/plugins/externalize-wp-packages.ts
+++ tools/vite-wp-react/src/plugins/externalize-wp-packages.ts
@@ -4,11 +4,10 @@ import viteExternalPlugin from 'vite-plugin-external';
 import {
 	BUNDLED_WP_PACKAGES,
 	NON_WP_PACKAGES,
-	WP_EXTERNAL_PACKAGES,
 	dashToCamelCase,
 } from '../utils/index.js';
 
-const viteExternal =
+const createExternal =
 	// viteExternalPlugin is not typed well
 	viteExternalPlugin as unknown as (typeof viteExternalPlugin)['default'];
 
@@ -25,9 +24,37 @@ function shouldExternalizePakage(name: string) {
 }
 
 /**
+ * Default externalize callback for WordPress packages.
+ * This will externalize all WordPress packages to `window.wp.*`
+ * and non-WordPress packages to their global variable names.
+ *
+ * @param name The package name.
+ * @returns The externalized variable name.
+ */
+export function defaultExternalizeCallback(name: string): string {
+	if (!shouldExternalizePakage(name)) {
+		return '';
+	}
+
+	if (name in NON_WP_PACKAGES) {
+		return NON_WP_PACKAGES[name];
+	}
+
+	if (name.startsWith('@wordpress/')) {
+		const variable = dashToCamelCase(name.replace('@wordpress/', ''));
+		return `wp.${variable}`;
+	}
+
+	return '';
+}
+
+/**
  * Updates the vite config to externalize all WordPress packages.
  */
 export const externalizeWpPackages = (): PluginOption => {
+	if ('development' === process.env.NODE_ENV) {
+		return [createExternal({ externals: defaultExternalizeCallback })];
+	}
 	return [
 		{
 			name: 'vwpr:externalize-wp-packages',
@@ -73,17 +100,5 @@ export const externalizeWpPackages = (): PluginOption => {
 				};
 			},
 		},
-		/**
-		 * We need this plugin only during development
-		 * To ensure that module aliases are created for external packages
-		 *
-		 * Change the externals list to dynamic if the below issue is resolved
-		 * @see https://github.com/fengxinming/vite-plugins/issues/44
-		 */
-		viteExternal({
-			development: {
-				externals: WP_EXTERNAL_PACKAGES,
-			},
-		}),
 	];
 };
