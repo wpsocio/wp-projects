ebc843a58a85873207f97746ce26b343b67906ae
Irshad Ahmad <irshad@wpsocio.com>
2025-04-02 22:33:11 +0530

    Improve vite wp react dependency externalization (#246)
    * Update vite-plugin-external to the latest RC
    
    * Use the externals callback for development
    
    * Allow passing a callback for externalization
    
    * Swap rollup callback with the default one
    
    * Finally do the clean up
    
    * Update and dedupe lockfile
    
    * Use Vite apply instead of process.env.NODE_ENV
    
    * Expose shouldExternalizePakage util
    
    * Add changeset
    
    * Remove react from dependencies as a proof
    
    We can't remove 'react' yet because we still need 'react/jsx-runtime' to be bundled.
    
    * Fix type checking
    
    * remove rollupOptions.external
    
    * Use interop: 'auto' and nuke rollup-plugin-external-globals

--- tools/vite-wp-react/src/plugins/externalize-wp-packages.ts
+++ tools/vite-wp-react/src/plugins/externalize-wp-packages.ts
@@ -1,89 +1,53 @@
-import rollupGlobals from 'rollup-plugin-external-globals';
 import type { PluginOption } from 'vite';
 import viteExternalPlugin from 'vite-plugin-external';
 import {
 	BUNDLED_WP_PACKAGES,
 	NON_WP_PACKAGES,
-	WP_EXTERNAL_PACKAGES,
 	dashToCamelCase,
 } from '../utils/index.js';
 
-const viteExternal =
+const createExternal =
 	// viteExternalPlugin is not typed well
 	viteExternalPlugin as unknown as (typeof viteExternalPlugin)['default'];
 
-function shouldExternalizePakage(name: string) {
+export function shouldExternalizePakage(name: string) {
 	if (BUNDLED_WP_PACKAGES.includes(name)) {
 		return false;
 	}
 
-	const isWpPackage = name.startsWith('@wordpress/');
+	return name in NON_WP_PACKAGES || name.startsWith('@wordpress/');
+}
 
-	const isNonWpPackage = name in NON_WP_PACKAGES;
+/**
+ * Default externalize callback for WordPress packages.
+ * This will externalize all WordPress packages to `window.wp.*`
+ * and non-WordPress packages to their global variable names.
+ *
+ * @param name The package name.
+ * @returns The externalized variable name.
+ */
+export function defaultExternalizeCallback(name: string): string {
+	if (!shouldExternalizePakage(name)) {
+		return '';
+	}
 
-	return isWpPackage || isNonWpPackage;
+	if (name in NON_WP_PACKAGES) {
+		return NON_WP_PACKAGES[name];
+	}
+
+	if (name.startsWith('@wordpress/')) {
+		const variable = dashToCamelCase(name.replace('@wordpress/', ''));
+		return `wp.${variable}`;
+	}
+
+	return '';
 }
 
 /**
  * Updates the vite config to externalize all WordPress packages.
  */
-export const externalizeWpPackages = (): PluginOption => {
-	return [
-		{
-			name: 'vwpr:externalize-wp-packages',
-			config(_, { command }) {
-				// We need to run this only for the build command
-				if (command !== 'build') {
-					return {};
-				}
-				return {
-					build: {
-						rollupOptions: {
-							external(source) {
-								return shouldExternalizePakage(source);
-							},
-							plugins: [
-								/**
-								 * Add the plugin to rollup to ensure react imports don't end up in the bundle
-								 * framer-motion causes the issue by using namespace imports
-								 *
-								 * @see https://github.com/vitejs/vite-plugin-react/issues/3
-								 */
-								rollupGlobals((name) => {
-									if (!shouldExternalizePakage(name)) {
-										return '';
-									}
-
-									if (name in NON_WP_PACKAGES) {
-										return NON_WP_PACKAGES[name];
-									}
-
-									if (name.startsWith('@wordpress/')) {
-										const variable = dashToCamelCase(
-											name.replace('@wordpress/', ''),
-										);
-										return `wp.${variable}`;
-									}
-
-									return '';
-								}),
-							],
-						},
-					},
-				};
-			},
-		},
-		/**
-		 * We need this plugin only during development
-		 * To ensure that module aliases are created for external packages
-		 *
-		 * Change the externals list to dynamic if the below issue is resolved
-		 * @see https://github.com/fengxinming/vite-plugins/issues/44
-		 */
-		viteExternal({
-			development: {
-				externals: WP_EXTERNAL_PACKAGES,
-			},
-		}),
-	];
+export const externalizeWpPackages = (
+	callback = defaultExternalizeCallback,
+): PluginOption => {
+	return [createExternal({ interop: 'auto', externals: callback })];
 };
--- tools/vite-wp-react/src/utils/wp-packages.ts
+++ tools/vite-wp-react/src/utils/wp-packages.ts
@@ -1,89 +1,10 @@
-import { dashToCamelCase } from './dash-to-camel-case.js';
-
-/**
- * WordPress packages that are bundled.
- * 
- * The list is the output of this snippet:
- * 
-$assets = include ABSPATH . WPINC . '/assets/script-loader-packages.php';
-print json_encode(
-	array_map(
-		fn( $asset ) => str_replace( '.js', '', $asset ),
-		array_keys( $assets )
-	),
-	JSON_PRETTY_PRINT
-);
- */
-export const WP_PACKAGES: Record<string, string> = Object.fromEntries(
-	[
-		'a11y',
-		'annotations',
-		'api-fetch',
-		'autop',
-		'blob',
-		'block-directory',
-		'block-editor',
-		'block-library',
-		'block-serialization-default-parser',
-		'blocks',
-		'commands',
-		'components',
-		'compose',
-		'core-commands',
-		'core-data',
-		'customize-widgets',
-		'data',
-		'data-controls',
-		'date',
-		'deprecated',
-		'dom',
-		'dom-ready',
-		'edit-post',
-		'edit-site',
-		'edit-widgets',
-		'editor',
-		'element',
-		'escape-html',
-		'format-library',
-		'hooks',
-		'html-entities',
-		'i18n',
-		'is-shallow-equal',
-		'keyboard-shortcuts',
-		'keycodes',
-		'list-reusable-blocks',
-		'media-utils',
-		'notices',
-		'nux',
-		'patterns',
-		'plugins',
-		'preferences',
-		'preferences-persistence',
-		'primitives',
-		'priority-queue',
-		'private-apis',
-		'redux-routine',
-		'reusable-blocks',
-		'rich-text',
-		'router',
-		'server-side-render',
-		'shortcode',
-		'style-engine',
-		'token-list',
-		'url',
-		'viewport',
-		'warning',
-		'widgets',
-		'wordcount',
-	].map((name) => [`@wordpress/${name}`, `wp.${dashToCamelCase(name)}`]),
-);
-
 export const NON_WP_PACKAGES: Record<string, string> = {
 	jquery: 'jQuery',
 	tinymce: 'tinymce',
 	moment: 'moment',
 	react: 'React',
 	'react-dom': 'ReactDOM',
+	'react-dom/client': 'ReactDOM',
 	backbone: 'Backbone',
 	lodash: 'lodash',
 	'lodash-es': 'lodash',
@@ -93,6 +14,7 @@ export const NON_WP_PACKAGES: Record<string, string> = {
 
 export const PACKAGE_HANDLES: Record<string, string> = {
 	'lodash-es': 'lodash',
+	'react-dom/client': 'react-dom',
 	// 'react/jsx-runtime': 'react-jsx-runtime',
 	// 'react-refresh/runtime': 'wp-react-refresh-runtime',
 };
@@ -105,14 +27,11 @@ export const PACKAGE_HANDLES: Record<string, string> = {
  */
 export const BUNDLED_WP_PACKAGES = [
 	'@wordpress/dataviews',
+	'@wordpress/dataviews/wp',
 	'@wordpress/icons',
 	'@wordpress/interface',
 	'@wordpress/sync',
 	'@wordpress/undo-manager',
+	'@wordpress/upload-media',
 	'@wordpress/fields',
 ];
-
-export const WP_EXTERNAL_PACKAGES: Record<string, string> = {
-	...NON_WP_PACKAGES,
-	...WP_PACKAGES,
-};
