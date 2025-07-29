7972d9b0b81313eb8c6b3237cf57a0c04e1758b7
Irshad Ahmad <irshad@wpsocio.com>
2025-07-29 19:30:00 +0530

    Update FieldsetAsLabel to add description

--- packages/js/ui/src/wp/fieldset-as-label/index.tsx
+++ packages/js/ui/src/wp/fieldset-as-label/index.tsx
@@ -2,21 +2,36 @@ import { __experimentalView as View } from '@wordpress/components';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 
-export type FieldsetAsLabelProps = {
+export type FieldsetAsLabelProps = React.HTMLAttributes<HTMLFieldSetElement> & {
 	label: React.ReactNode;
-	children?: React.ReactNode;
+	labelProps?: React.HTMLAttributes<HTMLLegendElement>;
+	description?: React.ReactNode;
 };
 
-export function FieldsetAsLabel({ label, children }: FieldsetAsLabelProps) {
+export function FieldsetAsLabel({
+	label,
+	children,
+	labelProps,
+	description,
+	...props
+}: FieldsetAsLabelProps) {
 	return (
-		<fieldset>
+		<fieldset {...props}>
 			<View
+				{...labelProps}
 				as="legend"
 				className={clsx('components-base-control__label', styles.label)}
 			>
 				{label}
 			</View>
 			<View>{children}</View>
+			{description ? (
+				<View
+					className={clsx('components-base-control__help', styles.description)}
+				>
+					{description}
+				</View>
+			) : null}
 		</fieldset>
 	);
 }
