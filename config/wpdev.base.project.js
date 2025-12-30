7e760719d30a6f30d732cc5c662b31283f544127
Irshad Ahmad <irshad@wpsocio.com>
2025-12-30 13:19:39 +0530

    Force PHP 8.0 in plugins

--- config/wpdev.base.project.js
+++ config/wpdev.base.project.js
@@ -19,7 +19,7 @@ export const getBundleConfig = ({ slug, key, version, textDomain }) => {
 				data: {
 					requirements: {
 						// When updating these values, also update them in the E2E test workflow
-						requiresPHP: '7.4',
+						requiresPHP: '8.0',
 						requiresAtLeast: '6.6',
 						testedUpTo: '6.9',
 					},
