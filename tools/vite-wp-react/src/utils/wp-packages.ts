29fbf9b4068d2c1e553c6ef4573b31eaa08c0a73
Irshad Ahmad <irshad@wpsocio.com>
2025-12-30 00:33:01 -0800

    Vite WP React: Update WP bundled and external packages list. (#297)

--- tools/vite-wp-react/src/utils/wp-packages.ts
+++ tools/vite-wp-react/src/utils/wp-packages.ts
@@ -1,4 +1,5 @@
 export const NON_WP_PACKAGES: Record<string, string> = {
+	'@babel/runtime/regenerator': 'regeneratorRuntime',
 	jquery: 'jQuery',
 	tinymce: 'tinymce',
 	moment: 'moment',
@@ -9,10 +10,12 @@ export const NON_WP_PACKAGES: Record<string, string> = {
 	lodash: 'lodash',
 	'lodash-es': 'lodash',
 	// 'react/jsx-runtime': 'ReactJSXRuntime',
+	// 'react/jsx-dev-runtime': 'ReactJSXRuntime',
 	// 'react-refresh/runtime': 'ReactRefreshRuntime',
 };
 
 export const PACKAGE_HANDLES: Record<string, string> = {
+	'@babel/runtime/regenerator': 'regenerator-runtime',
 	'lodash-es': 'lodash',
 	'react-dom/client': 'react-dom',
 	// 'react/jsx-runtime': 'react-jsx-runtime',
@@ -23,9 +26,10 @@ export const PACKAGE_HANDLES: Record<string, string> = {
  * WordPress packages that are bundled.
  *
  * This list comes from Gutenberg
- * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/dependency-extraction-webpack-plugin/lib/util.js#L6
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/dependency-extraction-webpack-plugin/lib/util.js
  */
 export const BUNDLED_WP_PACKAGES = [
+	'@wordpress/admin-ui',
 	'@wordpress/dataviews',
 	'@wordpress/dataviews/wp',
 	'@wordpress/icons',
@@ -34,4 +38,5 @@ export const BUNDLED_WP_PACKAGES = [
 	'@wordpress/undo-manager',
 	'@wordpress/upload-media',
 	'@wordpress/fields',
+	'@wordpress/views',
 ];
